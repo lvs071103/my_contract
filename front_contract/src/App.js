0d81509a079d15b99c83dabecbdca5d08e6ba045
周福荣 <122167504@qq.com>
2022-11-27 23:20:13 +0800

    20221127

--- front_contract/src/App.js
+++ front_contract/src/App.js
@@ -1,39 +1,54 @@
-import React, { Component } from 'react'
+import React, { Component, lazy, Suspense } from 'react'
 import { unstable_HistoryRouter as HistoryRouter, Route, Routes } from 'react-router-dom'
-import Login from '@/pages/Login'
-import GeekLayout from '@/pages/Layout'
 import AuthRoute from './components/AuthRoute'
 import './index.scss'
-import Home from './pages/Home'
-import User from './pages/User'
-import Contract from './pages/Contract'
 import { history } from '@/utils/history'
-import Group from '@/pages/Group'
-// import UseEffectTest from './tools/useEffectTest'
+// 按需导入
+
+const Login = lazy(() => import('@/pages/Login'))
+const GeekLayout = lazy(() => import('@/pages/Layout'))
+const Home = lazy(() => import('@/pages/Home'))
+const User = lazy(() => import('@/pages/User'))
+const Group = lazy(() => import('@/pages/Group'))
+const Contract = lazy(() => import('@/pages/Contract'))
 
 export default class App extends Component {
   render () {
     return (
       // 路由配置
       <HistoryRouter history={history}>
-        <Routes>
-          {/* 创建路由path和组件对应关系 */}
-          {/* Layout需要鉴权处理 */}
-          <Route path='/' element={
-            <AuthRoute>
-              <GeekLayout />
-            </AuthRoute>
-          }>
-            <Route index element={<Home />}></Route>
-            {/* <Route path='/accounts' element={<User />}> */}
-            <Route path='/accounts/user/list' element={<User />}></Route>
-            <Route path='/accounts/group/list' element={<Group />}></Route>
-            {/* </Route> */}
-            {/* <Route path='/test/' element={<UseEffectTest name='Jack.Z' />}></Route> */}
-            <Route path='/contract/' element={<Contract />}></Route>
-          </Route>
-          <Route path='/login/' element={<Login />}></Route>
-        </Routes>
+        <Suspense
+          fallback={
+            <div
+              style={{
+                textAlign: 'center',
+                marginTop: 200
+              }}
+            >
+              loading...
+            </div>
+          }
+        >
+          <Routes>
+            {/* 创建路由path和组件对应关系 */}
+            {/* Layout需要鉴权处理 */}
+            <Route path='/' element={
+              <AuthRoute>
+                <GeekLayout />
+              </AuthRoute>
+            }>
+              <Route index element={<Home />}></Route>
+              {/* <Route path='/accounts' element={<User />}> */}
+              <Route path='/accounts/user/list' element={<User />}></Route>
+              <Route path='/accounts/group/list' element={<Group />}></Route>
+              {/* </Route> */}
+              {/* <Route path='/test/' element={<UseEffectTest name='Jack.Z' />}></Route> */}
+              <Route path='/contract/' element={<Contract />}></Route>
+            </Route>
+            <Route path='/login/' element={<Login />}></Route>
+          </Routes>
+        </Suspense>
+
       </HistoryRouter>
     )
   }
