18d0b5dc355033b038265d7fd9035d514b0826a7
Furong.zhou(周福荣) <Furong.zhou@dominos.com.cn>
2022-12-01 19:46:49 +0800

    20221201

--- my_contract/accounts/models.py
+++
@@ -1,6 +0,0 @@
-# Create your models here.
-
-# class Profile(models.Model):
-#     user = models.OneToOneField(User,
-#                                 on_delete=models.CASCADE,
-#                                 related_name='profile')
